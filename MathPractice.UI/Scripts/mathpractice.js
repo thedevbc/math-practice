7f2a4286fdfb287aa0c4f633ce76b2f3bcb496c6
BMULFORD\Student <ben@benmulford.com>
2018-05-16 21:49:02 -0500

    001-create: added a score variable to track correct answers

--- MathPractice.UI/Scripts/mathpractice.js
+++ MathPractice.UI/Scripts/mathpractice.js
@@ -1,8 +1,14 @@
 ﻿//provides random addition and subtraction math problems to be solved; once an answer is entered, it is checked to see if it's correct.
-    var nbr1;
-    var nbr2;
-    var tempNbr;
-    var operatorNbr;
+var nbr1;
+var nbr2;
+var tempNbr;
+var operatorNbr;
+var score = 0;
+
+function generateProblem() {
+    document.getElementById('response').textContent = "";
+    document.getElementById('refresh').innerHTML = "";
+    document.getElementById('answer').value = "";
 
     nbr1 = Math.floor((Math.random() * 899) + 100);
     tempNbr = Math.floor((Math.random() * 899) + 100);
@@ -20,7 +26,7 @@
     while (tempNbr > nbr1 && operatorNbr == 1) {
         tempNbr = Math.floor((Math.random() * 899) + 100);
     }
-    
+
     nbr2 = tempNbr;
     //if (tempNbr < nbr1) {
     //    nbr2 = tempNbr;
@@ -29,36 +35,39 @@
     document.getElementById('nbr1').textContent = " " + nbr1;
     document.getElementById('operator').textContent = operator + " ";
     document.getElementById('nbr2').textContent = nbr2;
+    
+}
+/**********END Number generator*******/
+function checkAnswer() {
+    var answer = document.getElementById('answer').value;
 
-    function checkAnswer() {
-        var answer = document.getElementById('answer').value;
+    if (operatorNbr == 0) {
+        solution = nbr1 + nbr2;
+        console.log(solution);
+        if (answer == solution) {
+            document.getElementById('response').textContent = "RIGHT! GOOD JOB!";
+            document.getElementById('refresh').innerHTML = '<input id="newProb" type="button" class="btn btn-success" value="Get a new problem." onclick="generateProblem()" />';
+            score++;
+        }//end if
+        else {
+            document.getElementById('response').textContent = "Try Again."
+        }//end else
+    }//end if add
 
-        if (operatorNbr == 0) {
-            solution = nbr1 + nbr2;
-            console.log(solution);
-            if (answer == solution) {
-                document.getElementById('response').textContent = "RIGHT! GOOD JOB!"
-                document.getElementById('refresh').innerHTML = '<input id="newProb" type="button" class="btn btn-success" value="Get a new problem." onclick="location.reload()" />';
-                
-            }//end if
-            else {
-                document.getElementById('response').textContent = "Try Again."
-            }//end else
-        }//end if add
-
-        if (operatorNbr == 1) {
-            solution = nbr1 - nbr2;
-            console.log(solution);
-            if (solution != answer) {
-                document.getElementById('response').textContent = "Try Again."
-            }//end if
-            else {
-                document.getElementById('response').textContent = "RIGHT! GOOD JOB!"
-                document.getElementById('refresh').innerHTML = '<input id="newProb" type="button" class="btn btn-success" value="Get a new problem." onclick="location.reload()" />';
-                
-            }//end else
-        }
+    if (operatorNbr == 1) {
+        solution = nbr1 - nbr2;
+        console.log(solution);
+        if (solution != answer) {
+            document.getElementById('response').textContent = "Try Again."
+        }//end if
+        else {
+            document.getElementById('response').textContent = "RIGHT! GOOD JOB!";
+            document.getElementById('refresh').innerHTML = '<input id="newProb" type="button" class="btn btn-success" value="Get a new problem" onclick="generateProblem()" />';
+            score++;
+        }//end else
     }
+    document.getElementById('score').textContent = score;
+}
 
-    var btnChkAnswer = document.getElementById('chkAnswer');
-    btnChkAnswer.addEventListener('click', function () { checkAnswer() });
+var btnChkAnswer = document.getElementById('chkAnswer');
+btnChkAnswer.addEventListener('click', function () { checkAnswer() });
